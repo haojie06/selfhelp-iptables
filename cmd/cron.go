3e7c3e803030011d6940961e5f117ce9bde2a35e
haojie06 <haojie06@qq.com>
2021-09-01 10:02:46 +0800

    change output color

--- cmd/cron.go
+++ cmd/cron.go
@@ -1,10 +1,10 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/robfig/cron/v3"
 	"selfhelp-iptables-whitelist/config"
 	"selfhelp-iptables-whitelist/ipt"
-	"selfhelp-iptables-whitelist/utils"
 )
 
 func startCron() {
@@ -14,22 +14,22 @@ func startCron() {
 		// hh 每半小时重置 h 每小时重置 hd 每半天重置 d 每天重置 w 每周重置
 		switch resetInterval {
 		case "hh":
-			utils.CmdColorCyan.Println("开启每半小时重置")
+			fmt.Println("开启每半小时重置")
 			c.AddFunc("*/30 * * * *", ipt.ResetIPWhitelist)
 		case "h":
-			utils.CmdColorCyan.Println("开启每小时重置")
+			fmt.Println("开启每小时重置")
 			c.AddFunc("@hourly", ipt.ResetIPWhitelist)
 		case "hd":
-			utils.CmdColorCyan.Println("开启每半天重置")
+			fmt.Println("开启每半天重置")
 			c.AddFunc("0 0,12 * * *", ipt.ResetIPWhitelist)
 		case "d":
-			utils.CmdColorCyan.Println("开启每日重置")
+			fmt.Println("开启每日重置")
 			c.AddFunc("@daily", ipt.ResetIPWhitelist)
 		case "w":
-			utils.CmdColorCyan.Println("开启每周重置")
+			fmt.Println("开启每周重置")
 			c.AddFunc("@weekly", ipt.ResetIPWhitelist)
 		default:
-			utils.CmdColorYellow.Println("无效重置参数:", resetInterval)
+			fmt.Println("无效重置参数:", resetInterval)
 		}
 
 	}
